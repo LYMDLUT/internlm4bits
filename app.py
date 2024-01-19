a55127fdff343e61803f279cffa6eff63059acd9
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2024-01-19 19:39:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def load_model():
 
 def prepare_generation_config():
     with st.sidebar:
-        max_length = st.slider("Max Length", min_value=32, max_value=2048, value=2048)
+        max_length = st.slider("Max Length", min_value=8, max_value=32768, value=32768)
         top_p = st.slider("Top P", 0.0, 1.0, 0.8, step=0.01)
         temperature = st.slider("Temperature", 0.0, 1.0, 0.7, step=0.01)
         st.button("Clear Chat History", on_click=on_btn_click)
